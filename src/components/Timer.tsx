a40fce849f2df2da0a4199e49a63e8412b5b1ae3
Anderson Luís Furlan <alsfurlan@gmail.com>
2024-02-15 16:53:10 -0300

    Managing an interval with useRef and the effect cleanup function

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,23 +1,34 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useRef, useState } from 'react';
 import Container from './UI/Container';
 import { type Timer as TimerProps } from '../store/timers-context';
 
 export const Timer: FC<TimerProps> = ({ name, duration }: TimerProps) => {
   const durationInMilliseconds = duration * 1000;
-
   const [remainingTime, setRemainingTime] = useState(durationInMilliseconds);
+  const remainingTimeInSeconds = remainingTime / 1000;
+  const interval = useRef<number | null>(null);
 
-  setInterval(() => {
-    setRemainingTime(prevTime => prevTime - 50);
-  }, 50)
-  
+  if (remainingTimeInSeconds <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setRemainingTime((prevTime) => prevTime - 50);
+    }, 50);
+
+    interval.current = timer;
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <Container as='article'>
       <h2>{name}</h2>
       <p>
-        <progress max={durationInMilliseconds} value={remainingTime}/>
+        <progress max={durationInMilliseconds} value={remainingTime} />
       </p>
-      <p>{duration}</p>
+      <p>{remainingTimeInSeconds}</p>
     </Container>
   );
 };
