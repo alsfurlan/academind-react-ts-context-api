4dd476138c6e2f8b228e176a18a1155b464b6722
Anderson Luís Furlan <alsfurlan@gmail.com>
2024-02-16 17:20:06 -0300

    useEffect and its dependencies

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,26 +1,35 @@
 import { FC, useEffect, useRef, useState } from 'react';
 import Container from './UI/Container';
-import { type Timer as TimerProps } from '../store/timers-context';
+import {
+  useTimersContext,
+  type Timer as TimerProps,
+} from '../store/timers-context';
 
 export const Timer: FC<TimerProps> = ({ name, duration }: TimerProps) => {
   const durationInMilliseconds = duration * 1000;
   const [remainingTime, setRemainingTime] = useState(durationInMilliseconds);
   const remainingTimeInSeconds = remainingTime / 1000;
   const interval = useRef<number | null>(null);
+  const { isRunning } = useTimersContext();
 
   if (remainingTimeInSeconds <= 0 && interval.current) {
     clearInterval(interval.current);
   }
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      setRemainingTime((prevTime) => prevTime - 50);
-    }, 50);
+    let timer: number;
 
-    interval.current = timer;
+    if (isRunning) {
+      timer = setInterval(() => {
+        setRemainingTime((prevTime) => prevTime - 50);
+      }, 50);
+      interval.current = timer;
+    } else if (interval.current) {
+      clearInterval(interval.current);
+    }
 
     return () => clearInterval(timer);
-  }, []);
+  }, [isRunning]);
 
   return (
     <Container as='article'>
